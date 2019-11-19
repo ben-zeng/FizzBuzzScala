857098c6419fff5fef61bca87473eef92c2012cf
Ben Zeng <benzeng94@me.com>
2019-11-19 18:05:18 +0000

    refactored by using switch statement

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -12,18 +12,24 @@ object FizzBuzz {
     isDivisibleBy3(number) && isDivisibleBy5(number)
   }
 
-  def result(number: Int): String = {
-    var result = " "
-    if (isDivisibleBy15(number))
-      result = "FizzBuzz"
-    else if (isDivisibleBy3(number))
-      result = "Fizz"
-    else if (isDivisibleBy5(number))
-      result = "Buzz"
-    else
-      result = number.toString
-    result
-  }
+  //  def result(number: Int): String = {
+  //    var result = " "
+  //    if (isDivisibleBy15(number))
+  //      result = "FizzBuzz"
+  //    else if (isDivisibleBy3(number))
+  //      result = "Fizz"
+  //    else if (isDivisibleBy5(number))
+  //      result = "Buzz"
+  //    else
+  //      result = number.toString
+  //    result
+  //  }
 
+  def result(num: Int): String = (isDivisibleBy3(num), isDivisibleBy5(num)) match {
+    case (true, true) => "FizzBuzz"
+    case (true, false) => "Fizz"
+    case (false, true) => "Buzz"
+    case (false, false) => num.toString
+  }
 
 }
