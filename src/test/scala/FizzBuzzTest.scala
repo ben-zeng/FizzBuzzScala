56ec305d981c8c87b855c112b8657615d1a023db
Ben Zeng <benzeng94@me.com>
2019-11-19 14:55:17 +0000

    tests for divisible by 5

--- src/test/scala/FizzBuzzTest.scala
+++ src/test/scala/FizzBuzzTest.scala
@@ -10,4 +10,16 @@ class FizzBuzzTest extends FunSpec{
     }
   }
 
+  describe("#isDivisibleBy5") {
+    it("returns true if divisible by 3") {
+      assert(FizzBuzz.isDivisibleBy5(15) === true)
+    }
+    it("returns false if not divisible by 3") {
+      assert(FizzBuzz.isDivisibleBy5(7) === false)
+    }
+  }
+
+
+
+
 }
