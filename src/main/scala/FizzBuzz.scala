834db7b4d93c003274c555fe6508aedc24c45e41
Ben Zeng <benzeng94@me.com>
2019-11-19 14:48:55 +0000

    returns same number if not 3

---
+++ src/main/scala/FizzBuzz.scala
@@ -0,0 +1,8 @@
+object FizzBuzz {
+  def result(number: Int): String = {
+    if (number == 3)
+      "Fizz"
+    else
+      number.toString
+  }
+}
--- src/test/scala/FizzBuzzTest.scala
+++ src/test/scala/FizzBuzzTest.scala
@@ -3,7 +3,10 @@ import org.scalatest.FunSpec
 class FizzBuzzTest extends FunSpec{
   describe("#result") {
     it("should return 'fizz' when number is 3") {
-      assert Fizzbuzz.result(3) === "fizz"
+      assert(FizzBuzz.result(3) === "Fizz")
+    }
+    it("should return the same number when number is not 3") {
+      assert(FizzBuzz.result(5) === "5")
     }
   }
 }
